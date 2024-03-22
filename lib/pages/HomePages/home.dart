2fe7e194c8b262814520f6a1f5b5ee2f2755110c
sudeepreddy999 <145893583+sudeepreddy999@users.noreply.github.com>
2024-03-22 21:21:05 +0530

    added bottom tab view Co-authored-by: Nandu-25 <Nandu-25@users.noreply.github.com>

--- lib/pages/HomePages/home.dart
+++ lib/pages/HomePages/home.dart
@@ -1,7 +1,9 @@
+import 'package:fit_pal/utility/GlassMorphism.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:custom_sliding_segmented_control/custom_sliding_segmented_control.dart';
 import 'package:fit_pal/utility/date_picker_widget.dart';
+import 'package:fit_pal/pages/HomePages/segmentedcontrol.dart';
 import 'package:flutter/widgets.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -16,193 +18,268 @@ class _HomeState extends State<Home> {
   final PageController _pages1 = PageController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // duration: Duration(seconds: 1),
-      height: double.maxFinite,
-      width: double.maxFinite,
-      decoration: BoxDecoration(
-          gradient: RadialGradient(
-        radius: 1.5,
-        // center: Alignment(0, _pos.value),
-        center: Alignment(0, -1),
-        // radius:0.5,
-        colors: [Color.fromARGB(255, 123, 69, 232), Colors.white],
-        stops: [0.0, 1.0],
-      )),
-      // color: Colors.blue,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 120, left: 20, right: 20),
-            child: DatePicker(
-              DateTime.now(),
-              initialSelectedDate: DateTime.now(),
-              selectionColor: Colors.black,
-              selectedTextColor: Colors.white,
-              onDateChange: (date) {
-                // New date selected
-                setState(() {
-                  // _selectedValue = date;
-                });
-              },
+    return SingleChildScrollView(
+      child: Container(
+        // duration: Duration(seconds: 1),
+        height: double.maxFinite,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+          radius: 1.2,
+          // center: Alignment(0, _pos.value),
+          center: Alignment(0, -1),
+          // radius:0.5,
+          colors: [Color.fromARGB(255, 123, 69, 232), Colors.white],
+          stops: [0.0, 1.0],
+        )),
+        // color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 120, left: 20, right: 20),
+              child: DatePicker(
+                DateTime.now(),
+                initialSelectedDate: DateTime.now(),
+                selectionColor: Colors.black,
+                selectedTextColor: Colors.white,
+                onDateChange: (date) {
+                  // New date selected
+                  setState(() {
+                    // _selectedValue = date;
+                  });
+                },
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // page view for calorie intake and exit with an indicator
-          Container(
-            // color: Colors.blue,
-            // color: Colors.blue,
-            height: 250,
-            child: PageView(
-              controller: _pages1,
-              children: [
-                Container(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          '1286',
-                          style: TextStyle(
-                            height: 0.8,
-                            // fontFamily: 'Bebas_Neue',
-                            letterSpacing: -10,
-                            fontSize: 130,
-                            color: Colors.white.withOpacity(0.9),
-                            // backgroundColor: Colors.blue,
-                            fontWeight: FontWeight.w600,
+            const SizedBox(
+              height: 20,
+            ),
+            // page view for calorie intake and exit with an indicator
+            Container(
+              // color: Colors.blue,
+              // color: Colors.blue,
+              height: 250,
+              child: PageView(
+                controller: _pages1,
+                children: [
+                  Container(
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            '1286',
+                            style: TextStyle(
+                              height: 0.8,
+                              // fontFamily: 'Bebas_Neue',
+                              letterSpacing: -10,
+                              fontSize: 130,
+                              color: Colors.white.withOpacity(0.9),
+                              // backgroundColor: Colors.blue,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Kcal left',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
+                          const SizedBox(
+                            height: 10,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                  margin: EdgeInsets.all(10),
-                                  color: Colors.blue,
-                                  height: 80,
-                                  // width: 90,
-                                  child: Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text('Protein'),
-                                        Text('32%'),
-                                      ],
-                                    ),
-                                  )),
+                          Text(
+                            'Kcal left',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
                             ),
-                            Expanded(
-                              child: Container(
-                                  margin: EdgeInsets.all(10),
-                                  color: Colors.red,
-                                  height: 80,
-                                  // width: 90,
-                                  child: Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text('Carbs'),
-                                        Text('44%'),
-                                      ],
-                                    ),
-                                  )),
-                            ),
-                            Expanded(
-                              child: Container(
-                                  margin: EdgeInsets.all(10),
-                                  color: Colors.green,
-                                  height: 80,
-                                  // width: 90,
-                                  child: Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text('Fat'),
-                                        Text('28%'),
-                                      ],
-                                    ),
-                                  )),
-                            ),
-                          ],
-                        )
-                      ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: GlassMorphism(
+                                    blur: 50,
+                                    // margin: EdgeInsets.all(10),
+                                    // color: Colors.blue,
+                                    // height: 80,
+                                    // width: 90,
+                                    child: Center(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Protein',
+                                            style: TextStyle(
+                                              fontFamily: 'Roboto',
+                                              // color: Colors.blue,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          Text(
+                                            '32%',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color:
+                                                  Colors.black.withOpacity(0.7),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )),
+                              ),
+                              Expanded(
+                                child: GlassMorphism(
+                                    blur: 50,
+                                    // margin: EdgeInsets.all(10),
+                                    // color: Colors.red,
+                                    // height: 80,
+                                    // width: 90,
+                                    child: Center(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Carbs',
+                                            style: TextStyle(
+                                              fontFamily: 'Roboto',
+                                              // color: Colors.blue,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          Text(
+                                            '44%',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color:
+                                                  Colors.black.withOpacity(0.7),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )),
+                              ),
+                              Expanded(
+                                child: GlassMorphism(
+                                    blur: 50,
+                                    // margin: EdgeInsets.all(10),
+                                    // color: Colors.green,
+                                    // height: 80,
+                                    // width: 90,
+                                    child: Center(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Fat',
+                                            style: TextStyle(
+                                              fontFamily: 'Roboto',
+                                              // color: Colors.blue,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          Text(
+                                            '28%',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color:
+                                                  Colors.black.withOpacity(0.7),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          '1444',
-                          style: TextStyle(
-                            // fontFamily: 'Bebas_Neue',
-                            height: 0.8,
-                            letterSpacing: -10,
-                            fontSize: 130,
-                            color: Colors.white.withOpacity(0.9),
-                            fontWeight: FontWeight.w600,
+                  Container(
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            '1444',
+                            style: TextStyle(
+                              // fontFamily: 'Bebas_Neue',
+                              height: 0.8,
+                              letterSpacing: -10,
+                              fontSize: 130,
+                              color: Colors.white.withOpacity(0.9),
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Kcal to burn',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
+                          const SizedBox(
+                            height: 10,
                           ),
-                        ),
-                      ],
+                          Text(
+                            'Kcal to burn',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          SmoothPageIndicator(
-              controller: _pages1,
-              count: 2,
-              effect: const ScrollingDotsEffect(
-                // border: Border.all(color: Colors.grey,width: 1),
-                dotColor: Colors.white,
-                activeDotColor: Color.fromARGB(255, 169, 131, 239),
-                activeStrokeWidth: 2.6,
-                activeDotScale: 1.0,
-                maxVisibleDots: 5,
-                radius: 8,
-                spacing: 10,
-                dotHeight: 9,
-                dotWidth: 9,
-              )),
+            const SizedBox(
+              height: 10,
+            ),
 
-          // A tab view for meals, activity, water.
-        ],
+            SmoothPageIndicator(
+                controller: _pages1,
+                count: 2,
+                effect: const ScrollingDotsEffect(
+                  // border: Border.all(color: Colors.grey,width: 1),
+                  dotColor: Colors.white,
+                  activeDotColor: Color.fromARGB(255, 169, 131, 239),
+                  activeStrokeWidth: 2.6,
+                  activeDotScale: 1.0,
+                  maxVisibleDots: 5,
+                  radius: 8,
+                  spacing: 10,
+                  dotHeight: 9,
+                  dotWidth: 9,
+                )),
+            Container(
+              padding: EdgeInsets.only(left: 30, top: 12),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                textAlign: TextAlign.left,
+                "Daily Meals",
+                style: TextStyle(
+                  fontFamily: "roboto",
+                  color: Colors.black,
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            Container(
+              child: SegmentedControl(),
+            ),
+
+            // A tab view for meals, activity, water.
+          ],
+        ),
       ),
     );
   }
--- lib/utility/GlassMorphism.dart
+++ lib/utility/GlassMorphism.dart
@@ -3,29 +3,38 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class GlassMorphism extends StatelessWidget {
-  const GlassMorphism(
-      {super.key,
-      required this.blur,
-      required this.opacity,
-      required this.child});
+  const GlassMorphism({super.key, required this.blur, required this.child});
   final double blur;
-  final double opacity;
+  // final double opacity;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
+      // margin:const EdgeInsets.all(10),
+      // height: 80,
       borderRadius: BorderRadius.circular(20),
       child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
         child: Container(
           margin: const EdgeInsets.all(10),
           height: 80,
           decoration: BoxDecoration(
-              color: Colors.white.withOpacity((opacity)),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              border:
-                  Border.all(width: 1.5, color: Colors.white.withOpacity(0.2))),
+            // elevation:10,
+            color: Colors.white.withOpacity((0.3)),
+            // color: Colors.transparent,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.grey.withOpacity(0.3), // Shadow color
+            //     spreadRadius: 2, // Spread radius
+            //     blurRadius: 7, // Blur radius
+            //     offset: Offset(0, 3), // Offset
+            //   ),
+            // ],
+            // border:Border.all(width: 1.5, color: Colors.white.withOpacity(0.3))
+          ),
+          child: child,
         ),
       ),
     );
