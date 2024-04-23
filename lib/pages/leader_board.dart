453327e2e46dc2e8f8a8691d4243889849956dce
sudeepreddy999 <145893583+sudeepreddy999@users.noreply.github.com>
2024-04-24 01:47:31 +0530

    modified leaderboard page

--- lib/pages/leader_board.dart
+++ lib/pages/leader_board.dart
@@ -1,3 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fit_pal/DataBaseServices/Intialziedata.dart';
+import 'package:fit_pal/DataFriends/getfriends.dart';
+import 'package:fit_pal/pages/friend_search_page.dart';
 import 'package:flutter/material.dart';
 
 class LeaderBoard extends StatefulWidget {
@@ -8,6 +13,65 @@ class LeaderBoard extends StatefulWidget {
 }
 
 class _LeaderBoardState extends State<LeaderBoard> {
+  int XP = 0;
+  Future<int> getCurrentWeekXP() async {
+    // Get the start and end dates for the current week
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+    String userId = user!.uid;
+    final firestoreInstance = FirebaseFirestore.instance;
+    DateTime now = DateTime.now();
+    DateTime startDate = DateTime(now.year, now.month,
+        now.day - now.weekday + 1); // Start date of the current week (Monday)
+    DateTime endDate = startDate
+        .add(Duration(days: 6)); // End date of the current week (Sunday)
+
+    // Query Firestore for XP within the current week's range
+    DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await firestoreInstance.collection('XP').doc(userId).get();
+
+    // Initialize a map to store XP for each date in the current week
+    Map<String, int> currentWeekXP = {};
+
+    // Extract data from the document snapshot
+    Map<String, dynamic> data = documentSnapshot.data()!;
+
+    // Iterate over the data
+    data.forEach((key, value) {
+      DateTime date = DateTime.parse(key);
+      // Check if the date is within the current week's range
+      if (date.isAfter(startDate.subtract(Duration(days: 1))) &&
+          date.isBefore(endDate.add(Duration(days: 1)))) {
+        currentWeekXP[key] = value;
+      }
+    });
+    int s = 0;
+
+    DateTime date2 = DateTime.now();
+    String dateString = date2.toIso8601String().substring(0, 10);
+    currentWeekXP.forEach((date, xp) {
+      if (dateString == date) {
+        print("yes");
+        setState(() {
+          // todayXP = xp;
+        });
+      }
+      s += xp;
+    });
+    setState(() {
+      // weekXP = s;
+      XP = s;
+    });
+
+    return s;
+  }
+
+  @override
+  void initState() {
+    getCurrentWeekXP();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +92,7 @@ class _LeaderBoardState extends State<LeaderBoard> {
           children: [
             Container(
               padding: const EdgeInsets.only(top: 40),
-              height: 315,
+              height: 330,
               decoration: const BoxDecoration(
                 color: Color.fromARGB(255, 64, 220, 69),
                 borderRadius: BorderRadius.only(
@@ -59,8 +123,8 @@ class _LeaderBoardState extends State<LeaderBoard> {
                   const SizedBox(height: 5),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      ProfileStat(text: "45", label: "XP"),
+                    children: [
+                      ProfileStat(text: "${XP}", label: "XP"),
                       ProfileStat(text: "35", label: "Rank"),
                     ],
                   )
@@ -105,7 +169,92 @@ class ProfileStat extends StatelessWidget {
   }
 }
 
-class ExpandedListView extends StatelessWidget {
+class ExpandedListView extends StatefulWidget {
+  @override
+  State<ExpandedListView> createState() => _ExpandedListViewState();
+}
+
+class _ExpandedListViewState extends State<ExpandedListView> {
+  List<String> foundFriends = [];
+
+  List<CustomUser> usersfriends = [];
+
+  Future<int> getTotalXP(String userId) async {
+    // Get the start and end dates for the current week
+    final firestoreInstance = FirebaseFirestore.instance;
+    DateTime now = DateTime.now();
+    DateTime startDate = DateTime(now.year, now.month,
+        now.day - now.weekday + 1); // Start date of the current week (Monday)
+    DateTime endDate = startDate
+        .add(Duration(days: 6)); // End date of the current week (Sunday)
+
+    // Query Firestore for XP within the current week's range
+    DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await firestoreInstance.collection('XP').doc(userId).get();
+
+    // Initialize a map to store XP for each date in the current week
+    Map<String, int> currentWeekXP = {};
+
+    // Extract data from the document snapshot
+    Map<String, dynamic> data = documentSnapshot.data()!;
+    int sum = 0;
+    // Iterate over the data
+    data.forEach((key, value) {
+      // Check if the date is within the current week's range
+      sum += value as int;
+      currentWeekXP[key] = value;
+    });
+    int s = 0;
+
+    DateTime date2 = DateTime.now();
+    String dateString = date2.toIso8601String().substring(0, 10);
+    currentWeekXP.forEach((date, xp) {
+      if (dateString == date) {
+        print("yes");
+        setState(() {
+          // todayXP = xp;
+        });
+      }
+      s += xp;
+    });
+    setState(() {
+      // weekXP = s;
+    });
+
+    return sum;
+  }
+
+  Future<void> loadUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    String uid = user!.uid;
+    UserData userData = UserData(uid: uid, statusMap: {});
+    // names = await userData.getUserIdsWithStatusU();
+    // for (int i = 0; i < names.length; i++) {
+    //   String uid1 = names[i];
+    //   Dataservices dataservices = Dataservices(uid: uid1);
+    //   Map<String, dynamic> userinfo1 = await dataservices.getUserInfo();
+    //   int XP1 = await getTotalXP(uid1);
+    //   notusersfriends.add(CustomUser(
+    //       name: userinfo1['name'],
+    //       email: userinfo1['email'],
+    //       XP: XP1,
+    //       streak: 5));
+    // }
+    foundFriends = await userData.getUserIdsWithStatusF();
+    for (int i = 0; i < foundFriends.length; i++) {
+      String uid1 = foundFriends[i];
+      Dataservices dataservices = Dataservices(uid: uid1);
+      Map<String, dynamic> userinfo1 = await dataservices.getUserInfo();
+      int XP1 = await getTotalXP(uid1);
+      usersfriends.add(CustomUser(
+          name: userinfo1['name'],
+          email: userinfo1['email'],
+          XP: XP1,
+          streak: 5));
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
